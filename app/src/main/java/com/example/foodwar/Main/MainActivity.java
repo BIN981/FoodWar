5809537b1dc07612ef9a15e4821d4b0759b53972
nguyenhuubinh <binhnhhe153478@fpt.edu.vn>
2023-02-20 23:54:01 +0700

    /add_mainActivity

---
+++ app/src/main/java/com/example/foodwar/Main/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.foodwar.Main;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.foodwar.R;
+
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_profile);
+    }
+}
